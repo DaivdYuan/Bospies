67b10802c0dc1ad086a5e1ed07d62e55f1bff4f8
DaivdYuan <47169162+DaivdYuan@users.noreply.github.com>
2023-05-11 11:07:18 -0700

    style changes

--- bospies-app/src/components/SideBar.js
+++ bospies-app/src/components/SideBar.js
@@ -1,56 +1,57 @@
-import * as React from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import Typography from '@mui/material/Typography';
-import { Button } from '@mui/base';
-
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Typography from "@mui/material/Typography";
+import { Button } from "@mui/base";
 
 let listStyle = {
-    display: "flex",
-    width: "300px",
-    flexDirection: "column",
-    marginLeft: "60px"
-}
+  display: "flex",
+  width: "300px",
+  flexDirection: "column",
+  marginLeft: "60px",
+};
 
 let sideTitleStyle = {
-    borderBottom: "solid black",
-    display: "flex",
-    justifyContent: "space-between",
-    paddingLeft: "15px",
-}
+  borderBottom: "solid black",
+  display: "flex",
+  justifyContent: "space-between",
+  paddingLeft: "15px",
+};
 
 let addStyle = {
-    border: "none",
-    backgroundColor: "white",
-    fontSize: "25px",
-}
+  border: "none",
+  backgroundColor: "white",
+  fontSize: "25px",
+};
 
 let itemStyle = {
-    borderBottom: "solid gray",
-}
+  borderBottom: "solid gray",
+};
 
-const groupNames = ["BOSP Kyoto Fall ‘22", "BOSP Ambassadors", "Doshisha Fall ‘22"];
-const listItems = groupNames.map((groupName) =>
-    <ListItem style={itemStyle}>
-        <ListItemText primary={groupName} />
-    </ListItem>
-);
+const groupNames = [
+  "BOSP Kyoto Fall ‘22",
+  "BOSP Ambassadors",
+  "Doshisha Fall ‘22",
+];
+const listItems = groupNames.map((groupName) => (
+  <ListItem style={itemStyle}>
+    <ListItemText primary={groupName} />
+  </ListItem>
+));
 
 export default function SideBar() {
-    return (
-        <div style={listStyle}>
-            <div style={sideTitleStyle}>
-                <Typography sx={{ mt: 2, mb: 2 }} variant="h6">
-                    Groups
-                </Typography>
-                <Button style={addStyle}>+</Button>
-            </div>
-            <List>
-                {listItems}
-            </List>
+  return (
+    <div style={listStyle}>
+      <div style={sideTitleStyle}>
+        <Typography sx={{ mt: 2, mb: 2 }} variant="h6">
+          Groups
+        </Typography>
+        <Button style={addStyle}>+</Button>
+      </div>
+      <List>{listItems}</List>
 
-            {/* <div style={sideTitleStyle}>
+      {/* <div style={sideTitleStyle}>
                 <Typography sx={{ mt: 4, mb: 2 }} variant="h6">
                     Top Conversations
                 </Typography>
@@ -64,6 +65,6 @@ export default function SideBar() {
                     </ListItem>,
                 )}
             </List> */}
-        </div>
-    )
+    </div>
+  );
 }
