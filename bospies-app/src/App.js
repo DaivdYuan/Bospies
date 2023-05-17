943f68a9121dc0b432a6a3412a4ec4b5f0e82048
Julie Wang <douzhang@DNa1cc6f5.SUNet>
2023-05-16 21:00:28 -0700

    fixed merge issue

--- bospies-app/src/App.js
+++ bospies-app/src/App.js
@@ -6,7 +6,6 @@ import {
 import logo from './logo.svg';
 import './App.css';
 import CreatePostLayout from './layouts/CreatePostLayout'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomepageLayout from './layouts/HomepageLayout';
 import PostLayout from "./layouts/PostLayout";
 
